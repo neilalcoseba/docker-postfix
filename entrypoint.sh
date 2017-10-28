cd552f03f1ee820595292fcb2d408e74145a7e0a
Neil Alcoseba <neil_alcoseba@yahoo.com>
2017-10-28 19:22:59 +0800

    The default home directory is always docker regardless of different user

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ then
 	USER_NAME="docker"
 fi
 
-adduser --disabled-password --gecos '' $USER_NAME
+adduser --disabled-password --home /home/docker --gecos '' $USER_NAME
 echo "/.*/ $USER_NAME" >> /etc/postfix/virtual_address
 echo "/.*/ $USER_NAME" >> /etc/postfix/virtual_domains
 
@@ -18,6 +18,6 @@ postmap /etc/postfix/virtual_domains
 postconf -e mydestination=regexp:/etc/postfix/virtual_domains
 postconf -e virtual_alias_maps=regexp:/etc/postfix/virtual_address
 
-echo "$USER_NAME:{PLAIN}$USER_NAME:$(id -u $USER_NAME):$(id -g $USER_NAME)::/home/$USER_NAME::" >> /etc/dovecot/users
+echo "$USER_NAME:{PLAIN}$USER_NAME:$(id -u $USER_NAME):$(id -g $USER_NAME)::/home/docker::" >> /etc/dovecot/users
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
